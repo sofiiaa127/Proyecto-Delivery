8deac81b67d060eb6f25798e5647323078c46aa5
Jenifer Natalia Samboni Lara <jnsamboni@unimayor.edu.co>
2026-05-31 13:01:55 -0500

    feat: Crear clase Nodo para servir como componente base de estructuras enlazadas

--- src/estructuras/linear/Nodo.java
+++ src/estructuras/linear/Nodo.java
@@ -1,5 +1,42 @@
 package estructuras.linear;
 
-public class Nodo {
+
+//Guarda un dato y una referencia al siguiente nodo
+//@param <TipoDato> tipo de dato almacenado en el nodo
+
+public class Nodo<TipoDato> {
+
+    private TipoDato dato;
+    private Nodo<TipoDato> siguiente;
+
     
+    //Crea un nodo con un dato inicial
+    //@param dato valor que se almacenará en el nodo
+     
+    public Nodo(TipoDato dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+    public TipoDato obtenerDato() {
+        return dato;
+    }
+
+    
+    //Reemplaza el dato almacenado en el nodo
+    //@param dato nuevo valor para el nodo
+    
+    public void asignarDato(TipoDato dato) {
+        this.dato = dato;
+    }
+
+
+    public Nodo<TipoDato> obtenerSiguiente() {
+        return siguiente;
+    }
+
+
+    public void asignarSiguiente(Nodo<TipoDato> siguiente) {
+        this.siguiente = siguiente;
+    }
 }
