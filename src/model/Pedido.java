b90533b2f7408fa3190a1403eba01fd37b523812
Jose Arturo Avirama Cruz <aviramacruzjosearturo@gmail.com>
2026-06-01 19:54:12 -0500

    Correccion Pedido.java y Repartidor.java

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,17 +1,30 @@
 package model;
 
-public class Pedido {
+
+//Representa un pedido del sistema de delivery :
+//a mayor prioridad, mayor "valor", por lo que el pedido mas urgente sale primero.
+public class Pedido implements Comparable<Pedido> {
+
 
     private int id;
     private String descripcion;
-    // prioridad del 1 al 5, donde 5 es la mas urgente
     private int prioridad;
     private EstadoPedido estado;
     private String zonaDestino;
     private String horaIngreso;
     private boolean esVip;
 
-    public Pedido(int id, String descripcion, int prioridad, String zonaDestino, String horaIngreso, boolean esVip) {
+
+
+
+    private Repartidor repartidor;
+    private int minutosEntrega;
+
+
+    public Pedido(int id, String descripcion, int prioridad, String zonaDestino,
+    String horaIngreso, boolean esVip) {
+
+
         this.id = id;
         this.descripcion = descripcion;
         this.prioridad = prioridad;
@@ -19,45 +32,87 @@ public class Pedido {
         this.horaIngreso = horaIngreso;
         this.esVip = esVip;
         this.estado = EstadoPedido.PENDIENTE;
+        this.repartidor = null;
+        this.minutosEntrega = 0;
     }
+
 
     public int getId() {
         return id;
     }
 
+
     public String getDescripcion() {
         return descripcion;
     }
+
 
     public int getPrioridad() {
         return prioridad;
     }
 
+
     public EstadoPedido getEstado() {
         return estado;
     }
+
 
     public void setEstado(EstadoPedido estado) {
         this.estado = estado;
     }
 
+
     public String getZonaDestino() {
         return zonaDestino;
     }
+
 
     public String getHoraIngreso() {
         return horaIngreso;
     }
 
+
     public boolean isEsVip() {
         return esVip;
     }
 
-    @Override
+
+    public Repartidor getRepartidor() {
+        return repartidor;
+    }
+
+
+    public void setRepartidor(Repartidor repartidor) {
+        this.repartidor = repartidor;
+    }
+
+
+    public int getMinutosEntrega() {
+        return minutosEntrega;
+    }
+
+
+    public void setMinutosEntrega(int minutosEntrega) {
+        this.minutosEntrega = minutosEntrega;
+    }
+
+
+    /**
+     * Compara por prioridad para el heap de la cola de prioridad.
+     * Devuelve positivo cuando este pedido es mas urgente que el otro,
+     * de modo que extraerMaximo() entregue siempre el mas prioritario.
+     */
+    public int compareTo(Pedido otro) {
+        return Integer.compare(this.prioridad, otro.prioridad);
+    }
+
+
     public String toString() {
         return "Pedido{id=" + id + ", descripcion='" + descripcion + "', prioridad=" + prioridad +
-               ", estado=" + estado + ", zona='" + zonaDestino + "', hora='" + horaIngreso + "', vip=" + esVip + "}";
+        ", estado=" + estado + ", zona='" + zonaDestino + "', hora='" + horaIngreso + 
+        "', vip=" + esVip + "}";
     }
 }
+
 
 //Arturo Avirama
--- src/model/Repartidor.java
+++ src/model/Repartidor.java
@@ -1,11 +1,14 @@
 package model;
 
+
 public class Repartidor {
+
 
     private String id;
     private String nombre;
     private boolean libre;
     private String zonaActual;
+
 
     public Repartidor(String id, String nombre, String zonaActual) {
         this.id = id;
@@ -14,33 +17,40 @@ public class Repartidor {
         this.libre = true;
     }
 
+
     public String getId() {
         return id;
     }
+
 
     public String getNombre() {
         return nombre;
     }
 
+
     public boolean isLibre() {
         return libre;
     }
+
 
     public void setLibre(boolean libre) {
         this.libre = libre;
     }
 
+
     public String getZonaActual() {
         return zonaActual;
     }
+
 
     public void setZonaActual(String zonaActual) {
         this.zonaActual = zonaActual;
     }
 
-    @Override
+
     public String toString() {
-        return "Repartidor{id='" + id + "', nombre='" + nombre + "', libre=" + libre + ", zona='" + zonaActual + "'}";
+        return "Repartidor{id='" + id + "', nombre='" + nombre + 
+        "', libre=" + libre + ", zona='" + zonaActual + "'}";
     }
 }
 
